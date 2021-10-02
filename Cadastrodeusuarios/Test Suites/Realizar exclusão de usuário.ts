e7953f963cf853f01a13348753c170938f7f81cb
matheussneves <37849598+matheussneves@users.noreply.github.com>
2021-10-02 00:04:59 -0300

    Create Realizar exclusão de usuário.ts

---
+++ Cadastrodeusuarios/Test Suites/Realizar exclusão de usuário.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Dado que acesso a página de cadastro.
+Quando aciono a opção excluir de um usuário exibido na tabela Usuários cadastrados.
+Então devo ter o usuário excluído da tabela Usuários cadastrados.
+E devo visualizar os demais usuários com os mesmos ID de quando cadastrados.</description>
+   <name>Realizar exclusão de usuário</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5cb936a9-f7a1-49b4-ac48-c7c3f0ca7838</testSuiteGuid>
+   <testCaseLink>
+      <guid>125606c6-0af7-4858-b069-80297a79e85f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Acessar tela de cadastro de usuários</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>acd3a92d-6c78-4912-b549-0f260ef2edd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>31510819-5887-4e18-bc61-167d67594117</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Cadastrar usuário</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ce2eeb36-8a61-4744-9189-987545b57dda</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>69b1503f-7827-4185-8a23-c0a3ddd1d4e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b1e3e1a8-0bdb-4e68-a2d7-0d26c0fb0d9b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>663a9786-64a2-4dfc-9fff-65e231b96109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c50925cf-5b00-4ab4-b7af-2b534c1f2346</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6d0bbd0-2660-45f7-b9a8-0c05ffe71943</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Cadastrar usuário</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ce2eeb36-8a61-4744-9189-987545b57dda</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>69b1503f-7827-4185-8a23-c0a3ddd1d4e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b1e3e1a8-0bdb-4e68-a2d7-0d26c0fb0d9b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>663a9786-64a2-4dfc-9fff-65e231b96109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c50925cf-5b00-4ab4-b7af-2b534c1f2346</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>86bd883d-4838-445f-badf-60179c3ce88c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Excluir usuario</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

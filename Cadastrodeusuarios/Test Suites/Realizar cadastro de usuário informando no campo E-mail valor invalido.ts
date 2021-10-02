955c5fd8a145fa45cf78a2a0a554b95daddd6858
matheussneves <37849598+matheussneves@users.noreply.github.com>
2021-10-02 00:04:33 -0300

    Create Realizar cadastro de usuário informando no campo E-mail valor invalido.ts

---
+++ Cadastrodeusuarios/Test Suites/Realizar cadastro de usuário informando no campo E-mail valor invalido.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Dado que acesso a página de cadastro.
+Quando submeto o meu cadastro sem o e-mail.
+Então devo ver abaixo do campo a mensagem informativa em vermelho &quot;O campo E-mail é obrigatório.&quot;
+E visualizar a tabela Usuários cadastrados preenchida com o usuário anteriormente cadastrado</description>
+   <name>Realizar cadastro de usuário informando no campo E-mail valor invalido</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b7bbd132-f51f-4b23-a272-bea49fc8f71f</testSuiteGuid>
+   <testCaseLink>
+      <guid>c500a2e9-9a39-4932-8669-10490797dd64</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Acessar tela de cadastro de usuários</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>acd3a92d-6c78-4912-b549-0f260ef2edd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>505dda16-2511-4669-bbae-168e58c998d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Cadastrar usuário</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ce2eeb36-8a61-4744-9189-987545b57dda</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>69b1503f-7827-4185-8a23-c0a3ddd1d4e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b1e3e1a8-0bdb-4e68-a2d7-0d26c0fb0d9b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>663a9786-64a2-4dfc-9fff-65e231b96109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c50925cf-5b00-4ab4-b7af-2b534c1f2346</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>

1bb1575c63fdda04eeb6cc92cbd7143c5b6a5b07
matheussneves <37849598+matheussneves@users.noreply.github.com>
2021-10-02 00:04:52 -0300

    Create Realizar cadastro de usuário sem informar o campo Senha.ts

---
+++ Cadastrodeusuarios/Test Suites/Realizar cadastro de usuário sem informar o campo Senha.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Dado que acesso a página de cadastro.
+Quando submeto o meu cadastro sem a senha.
+Então devo ver abaixo do campo a mensagem informativa em vermelho &quot;O campo Senha é obrigatório.&quot;</description>
+   <name>Realizar cadastro de usuário sem informar o campo Senha</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>43a7b391-a97e-4894-8244-f57e91c839ea</testSuiteGuid>
+   <testCaseLink>
+      <guid>9b6ccd11-7572-4594-881d-dd794ace1a0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Acessar tela de cadastro de usuários</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>acd3a92d-6c78-4912-b549-0f260ef2edd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c1e6f492-4f03-4793-8cd3-b18543b25be0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Cadastrar usuário</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ce2eeb36-8a61-4744-9189-987545b57dda</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>69b1503f-7827-4185-8a23-c0a3ddd1d4e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b1e3e1a8-0bdb-4e68-a2d7-0d26c0fb0d9b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>663a9786-64a2-4dfc-9fff-65e231b96109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c50925cf-5b00-4ab4-b7af-2b534c1f2346</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>

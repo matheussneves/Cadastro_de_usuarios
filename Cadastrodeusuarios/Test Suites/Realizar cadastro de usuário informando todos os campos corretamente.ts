996ebe6dd7946fc8cf9c048e029a442e49063a86
matheussneves <37849598+matheussneves@users.noreply.github.com>
2021-10-02 00:04:42 -0300

    Create Realizar cadastro de usuário informando todos os campos corretamente.ts

---
+++ Cadastrodeusuarios/Test Suites/Realizar cadastro de usuário informando todos os campos corretamente.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Dado que acesso a página de cadastro.
+Quando submeto o meu cadastro com nome, e-mail e senha corretos.
+Então devo ser cadastrado.
+E visualizar a tabela Usuários cadastrados preenchida com o usuário anteriormente cadastrado e os meus dados inseridos conforme tela 10 do escopo.</description>
+   <name>Realizar cadastro de usuário informando todos os campos corretamente</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9770b746-4921-4075-8dbc-7cf167ce819e</testSuiteGuid>
+   <testCaseLink>
+      <guid>9658f6e8-fb79-4a95-a794-32646d40df36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Acessar tela de cadastro de usuários</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>acd3a92d-6c78-4912-b549-0f260ef2edd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69a03681-2a84-4bec-ab01-05beecb8c6b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Cadastrar usuário</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ce2eeb36-8a61-4744-9189-987545b57dda</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>69b1503f-7827-4185-8a23-c0a3ddd1d4e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b1e3e1a8-0bdb-4e68-a2d7-0d26c0fb0d9b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>663a9786-64a2-4dfc-9fff-65e231b96109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c50925cf-5b00-4ab4-b7af-2b534c1f2346</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>

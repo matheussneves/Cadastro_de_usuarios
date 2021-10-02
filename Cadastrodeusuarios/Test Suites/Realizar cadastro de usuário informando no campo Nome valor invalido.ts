92ad378da292b390302ce9b99f2cba04e043df8e
matheussneves <37849598+matheussneves@users.noreply.github.com>
2021-10-02 00:04:30 -0300

    Create Realizar cadastro de usuário informando no campo Nome valor invalido.ts

---
+++ Cadastrodeusuarios/Test Suites/Realizar cadastro de usuário informando no campo Nome valor invalido.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Dado que acesso a página de cadastro.
+Quando submeto o meu cadastro sem o nome.
+Então devo ver abaixo no campo a mensagem informativa em vermelho &quot;O campo Nome é obrigatório.&quot;
+E visualizar a tabela Usuários cadastrados preenchida com o usuário anteriormente cadastrado</description>
+   <name>Realizar cadastro de usuário informando no campo Nome valor invalido</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4b3f4d79-09a7-4da3-b75f-fa054923e454</testSuiteGuid>
+   <testCaseLink>
+      <guid>e14df7dc-9e31-477e-8842-bda9ad8a063c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Acessar tela de cadastro de usuários</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>acd3a92d-6c78-4912-b549-0f260ef2edd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f227a897-de10-463b-b54e-db1393a17314</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Cadastrar usuário</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ce2eeb36-8a61-4744-9189-987545b57dda</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>69b1503f-7827-4185-8a23-c0a3ddd1d4e3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b1e3e1a8-0bdb-4e68-a2d7-0d26c0fb0d9b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>663a9786-64a2-4dfc-9fff-65e231b96109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c50925cf-5b00-4ab4-b7af-2b534c1f2346</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>

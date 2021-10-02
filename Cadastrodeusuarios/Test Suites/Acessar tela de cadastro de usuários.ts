f554e1487363fcc3ac945eb309badfc3966863c4
matheussneves <37849598+matheussneves@users.noreply.github.com>
2021-10-02 00:05:07 -0300

    Create Acessar tela de cadastro de usuários.ts

---
+++ Cadastrodeusuarios/Test Suites/Acessar tela de cadastro de usuários.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Dado que sou usuário do sistema.
+Quando Acesso a tela de cadastro de usuários.
+Então devo visualizar a página conforme tela 1 do escopo, contendo os campos Nome, E-mail, Senha e o botão Cadastrar.
+E não visualizando a tabela Usuários cadastrados.</description>
+   <name>Acessar tela de cadastro de usuários</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5627a424-876e-4414-9324-a019bbe16888</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a496ad4-7ee8-4ba2-b0a6-4844f14554c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Acessar tela de cadastro de usuários</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>acd3a92d-6c78-4912-b549-0f260ef2edd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>360ebed0-e513-40e1-a343-73d96c3e0814</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Página Cadastro de usúarios/Validar campos</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
